33dba1199a3836879710980f6ad5111a3ec96902
Alex9521 <romulopereiraalmeida@gmail.com>
2025-08-19 22:21:29 -0300

    add >= if else

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function generateNumber() {
   const min = Math.ceil(document.querySelector(".input-min").value);
   const max = Math.floor(document.querySelector(".input-max").value);
-  const result = Math.floor(
+  if (min >= max) {
+    alert("Minimum value cannot be greater than maximum value.");
+    return;
+  } else {const result = Math.floor(
     Math.random() * (max - min + 1) + min
   );
   alert(result);
 }
+  
+}
+
